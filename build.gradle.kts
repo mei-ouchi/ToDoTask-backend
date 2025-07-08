592c6ba966e400b7f551051e0ca4fc06c9f8e93b
EXCITE-2108-02\mei ouchi <mei.ouchi@excite-software.net>
2025-07-08 09:12:16 +0900

    feat: MyBatis Generatorを用いた自動生成

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.mybatis.generator") version "1.4.2"
 }
 
 group = "com.example"
@@ -24,16 +25,26 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("com.h2database:h2")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
+	implementation("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+	mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.2")
+	mybatisGenerator("com.h2database:h2:2.3.232")
+}
+
+mybatisGenerator {
+    verbose = true
+    overwrite = true
+    configurationFile = "src/main/resources/generatorConfig.xml"
 }
 
 tasks.withType<Test> {
