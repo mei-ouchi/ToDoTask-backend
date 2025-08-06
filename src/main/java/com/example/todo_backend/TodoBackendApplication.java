8f97da6aab48c2d9fa25555277682420b0841d80
EXCITE-2108-02\mei ouchi <mei.ouchi@excite-software.net>
2025-08-06 17:08:24 +0900

    fix: 冗長なMyBatisConfig.javaを削除

--- src/main/java/com/example/todo_backend/TodoBackendApplication.java
+++ src/main/java/com/example/todo_backend/TodoBackendApplication.java
@@ -1,9 +1,11 @@
 package com.example.todo_backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.todo_backend.infrastructure.mapper")
 public class TodoBackendApplication {
 
 	public static void main(String[] args) {
